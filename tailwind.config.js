e9a23ba8c44109f736a3dcf3a1ed47659767d070
fadlimaghfirli <fadlimaghfirli5@gmail.com>
2026-03-17 03:27:00 +0700

    inisialisasi tema warna dan tipografi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: "class", // Wajib ditambahkan untuk fitur switch gelap/terang
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                // Menambahkan custom font kita
+                "space-grotesk": [
+                    '"Space Grotesk"',
+                    ...defaultTheme.fontFamily.sans,
+                ],
+                roboto: ["Roboto", ...defaultTheme.fontFamily.sans],
+                mono: ['"Fira Code"', ...defaultTheme.fontFamily.mono],
             },
         },
     },
